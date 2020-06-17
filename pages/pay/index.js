3abd2516d78d99f7abf4eedb6bd5166966446e53
wangqilong1225 <664660754@qq.com>
2020-06-17 22:54:41 +0800

    支付页面

--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,66 +1,47 @@
-// pages/pay/index.js
+/*
+  1.页面加载的时候
+    1.从缓存中获取购物车数据 渲染到页面中
+      这些数据 checked=true
+
+  2.微信支付
+    1.哪些人 哪些账号可以支付
+    
+*/
+
+import { getSetting, chooseAddress, openSetting, showModel, showTost } from "../../utils/asyncWx.js"
+import regeneratorRuntime from "../../lib/runtime/runtime"
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    address: {},
+    cart: [],
+    totalPrice: 0,
+    totalNum: 0
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  onShow() {
+    //获取地址信息
+    const address = wx.getStorageSync('address');
+    //获取购物车信息
+    let cart = wx.getStorageSync('cart') || [];
 
-  },
+    //过滤checked=true的购物车数据
+    cart = cart.filter(v => v.checked);
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    //总价格 总数量
+    let totalPrice = 0;
+    let totalNum = 0;
 
-  },
+    cart.forEach(v => {
+      totalPrice += v.num * v.goods_price;
+      totalNum += v.num;
+    });
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    this.setData({
+      address: address,
+      cart: cart,
+      totalPrice: totalPrice,
+      totalNum: totalNum
+    });
   }
 })
